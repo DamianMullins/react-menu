2acc7cd0b33f73e8f7d0a6d79ef213cbd8cc1fc0
Damian Mullins <damian.mullins@just-eat.com>
2018-07-27 23:07:33 +0100

    Formatting.

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -6,11 +6,12 @@ const MenuItem = ({ item, onAddItemClick }) => (
             <p className="c-listing-item-text" v-html="item.name" dangerouslySetInnerHTML={{ __html: item.name }}></p>
             <p className="c-listing-item-text">{item.price}</p>
         </div>
+
         <div className="g-col g-span3 text-right">
             <button className="o-btn o-btn--primary o-btn--small" onClick={onAddItemClick}>+
-                        <span
-                            className="is-visuallyHidden"
-                            dangerouslySetInnerHTML={{ __html: `Add ${item.name} to basket` }}></span>
+                <span
+                    className="is-visuallyHidden"
+                    dangerouslySetInnerHTML={{ __html: `Add ${item.name} to basket` }}></span>
             </button>
         </div>
     </section>
